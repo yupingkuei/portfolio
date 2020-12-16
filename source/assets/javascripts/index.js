7ae5adc1975ed72f7b6ea43728dcfd11efa0931e
Yuping Kuei <kueiyuping@gmail.com>
2020-12-16 23:59:34 +0900

    add the gsap to social icons

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,3 +1,8 @@
 import { showMenu } from "./components/menu-icon.js";
 
 showMenu();
+const socialIcons = document.querySelector(".social-icons");
+console.log(socialIcons);
+const tl = new TimelineMax();
+console.log(tl);
+tl.fromTo(socialIcons, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 });
