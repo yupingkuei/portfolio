438cace1a3b0fc66a20f6e6a40c232ad6727f75e
Yuping Kuei <kueiyuping@gmail.com>
2020-12-17 01:10:16 +0900

    added the JS to header

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,8 +1,18 @@
 import { showMenu } from "./components/menu-icon.js";
 
 showMenu();
+const header = document.querySelector(".header");
+header.addEventListener("scroll", () => {
+  console.log("hi");
+});
+// gsap
 const socialIcons = document.querySelector(".social-icons");
-console.log(socialIcons);
+const menu = document.querySelector("#menu");
+const headerRight = document.querySelector(".header-right");
+const headerH1 = document.querySelector("h1");
+
 const tl = new TimelineMax();
-console.log(tl);
-tl.fromTo(socialIcons, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 });
+tl.fromTo(socialIcons, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 })
+  .fromTo(menu, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=1")
+  .fromTo(headerRight, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -220 }, "-=1")
+  .fromTo(headerH1, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -80 }, "-=1");
