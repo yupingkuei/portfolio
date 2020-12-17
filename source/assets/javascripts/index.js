df79bb401c739b2b3be57276fa5c8dbafa2aba51
Yuping Kuei <kueiyuping@gmail.com>
2020-12-17 18:03:03 +0900

    added the footer

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,12 +1,16 @@
 import { showMenu } from "./components/menu-icon.js";
 
 showMenu();
-const header = document.querySelector(".header");
-header.addEventListener("scroll", () => {
-  console.log("hi");
-});
+
+// const header = document.querySelector(".header");
+// document.addEventListener("scroll", (event) => {
+//   if (event.scrollTop() > 800) {
+//     console.log("hi");
+//   }
+// });
+
 // gsap
-const socialIcons = document.querySelector(".social-icons");
+const socialIcons = document.querySelector("#social-icons");
 const menu = document.querySelector("#menu");
 const headerRight = document.querySelector(".header-right");
 const headerH1 = document.querySelector("h1");
@@ -17,5 +21,5 @@ tl.fromTo(headerRight, 0.2, { opacity: 1, x: 0 }, { opacity: 1, x: 0 })
   .fromTo(headerRight, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -220 })
   .fromTo(headerH1, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -80 }, "-=1")
   .fromTo(intro, 1, { opacity: 0, y: 30 }, { opacity: 1, y: 0 })
-  .fromTo(socialIcons, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 })
-  .fromTo(menu, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=1");
+  .fromTo(menu, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 })
+  .fromTo(socialIcons, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=1");
