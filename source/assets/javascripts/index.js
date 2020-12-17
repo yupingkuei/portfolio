46a8c0a387aca9794670faf3f99db5b65ffe212a
Yuping Kuei <kueiyuping@gmail.com>
2020-12-17 16:30:53 +0900

    added the contact form

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -13,7 +13,8 @@ const headerH1 = document.querySelector("h1");
 const intro = document.querySelector(".intro");
 
 const tl = new TimelineMax();
-tl.fromTo(headerRight, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -220 })
+tl.fromTo(headerRight, 0.2, { opacity: 1, x: 0 }, { opacity: 1, x: 0 })
+  .fromTo(headerRight, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -220 })
   .fromTo(headerH1, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -80 }, "-=1")
   .fromTo(intro, 1, { opacity: 0, y: 30 }, { opacity: 1, y: 0 })
   .fromTo(socialIcons, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 })
