fa2dab33b0d1385ef93001f1d926c8fbea33cfc4
Yuping Kuei <kueiyuping@gmail.com>
2020-12-17 13:11:33 +0900

    changed the header animation

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -10,9 +10,11 @@ const socialIcons = document.querySelector(".social-icons");
 const menu = document.querySelector("#menu");
 const headerRight = document.querySelector(".header-right");
 const headerH1 = document.querySelector("h1");
+const intro = document.querySelector(".intro");
 
 const tl = new TimelineMax();
-tl.fromTo(socialIcons, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 })
-  .fromTo(menu, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=1")
-  .fromTo(headerRight, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -220 }, "-=1")
-  .fromTo(headerH1, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -80 }, "-=1");
+tl.fromTo(headerRight, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -220 })
+  .fromTo(headerH1, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -80 }, "-=1")
+  .fromTo(intro, 1, { opacity: 0, y: 30 }, { opacity: 1, y: 0 })
+  .fromTo(socialIcons, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 })
+  .fromTo(menu, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=1");
