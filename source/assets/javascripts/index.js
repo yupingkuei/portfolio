53f924a0920c0865535f756f983af3a71d6b8669
Yuping Kuei <kueiyuping@gmail.com>
2020-12-21 16:46:51 +0900

    added iru screenshots

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,9 +1,6 @@
 import { showMenu } from "./components/menu-icon.js";
 
 showMenu();
-$(".carousel").carousel({
-  interval: 2000,
-});
 
 // const header = document.querySelector(".header");
 // document.addEventListener("scroll", (event) => {
