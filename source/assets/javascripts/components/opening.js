a981dc4b0518b321c6dc754f1c2facd4ddc44fd2
Yuping Kuei <kueiyuping@gmail.com>
2020-12-22 14:30:10 +0900

    implemented the typing javascript

---
+++ source/assets/javascripts/components/opening.js
@@ -0,0 +1,17 @@
+// gsap
+const opening = () => {
+  const socialIcons = document.querySelector("#social-icons");
+  const menu = document.querySelector("#menu");
+  const headerRight = document.querySelector(".header-right");
+  const headerH1 = document.querySelector("h1");
+  const intro = document.querySelector(".intro");
+  const tl = new TimelineMax();
+  tl.fromTo(headerRight, 0.2, { opacity: 1, x: 0 }, { opacity: 1, x: 0 })
+    .fromTo(headerRight, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -220 })
+    .fromTo(headerH1, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -80 }, "-=1")
+    .fromTo(intro, 1, { opacity: 0, y: 30 }, { opacity: 1, y: 0 })
+    .fromTo(menu, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 })
+    .fromTo(socialIcons, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=1");
+};
+
+export { opening };
--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,6 +1,8 @@
 import { showMenu } from "./components/menu-icon.js";
+import { opening } from "./components/opening.js";
 
 showMenu();
+opening();
 
 // const header = document.querySelector(".header");
 // document.addEventListener("scroll", (event) => {
@@ -9,17 +11,31 @@ showMenu();
 //   }
 // });
 
-// gsap
-const socialIcons = document.querySelector("#social-icons");
-const menu = document.querySelector("#menu");
-const headerRight = document.querySelector(".header-right");
-const headerH1 = document.querySelector("h1");
-const intro = document.querySelector(".intro");
+//typing effect
 
-const tl = new TimelineMax();
-tl.fromTo(headerRight, 0.2, { opacity: 1, x: 0 }, { opacity: 1, x: 0 })
-  .fromTo(headerRight, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -220 })
-  .fromTo(headerH1, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -80 }, "-=1")
-  .fromTo(intro, 1, { opacity: 0, y: 30 }, { opacity: 1, y: 0 })
-  .fromTo(menu, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 })
-  .fromTo(socialIcons, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=1");
+const texts = [
+  "Web Developer",
+  "Project Manager",
+  "Digital Artist",
+  "Native Chinese Speaker",
+];
+let count = 0; //count of words
+let index = 0; //index of characters
+let currentText = "";
+let letter = "";
+
+const type = () => {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing").textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 200);
+};
+type();
