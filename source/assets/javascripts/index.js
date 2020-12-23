0e7a3b30834c205880862b50c4754317e31334a4
Yuping Kuei <kueiyuping@gmail.com>
2020-12-24 01:13:59 +0900

    responsiveness of header

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -5,6 +5,15 @@ import { type } from "./components/typing.js";
 showMenu();
 if (window.innerWidth > 479) {
   opening();
+} else {
+  const headerRight = document.querySelector(".header-right");
+  const headerH1 = document.querySelector("h1");
+  const intro = document.querySelector(".intro");
+  const tl = new TimelineMax();
+  tl.fromTo(headerRight, 0.2, { opacity: 1, x: 0 }, { opacity: 1, x: 0 })
+    .fromTo(headerRight, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -50 })
+    .fromTo(headerH1, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -80 }, "-=1")
+    .fromTo(intro, 1, { opacity: 0, y: 30 }, { opacity: 1, y: 0 });
 }
 
 type();
