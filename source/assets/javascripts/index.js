6975b0ff5fd963e86ed8055beb63c3c1d2fff0c3
Yuping Kuei <kueiyuping@gmail.com>
2020-12-24 01:45:45 +0900

    added 500px+ responsiveness

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -3,17 +3,32 @@ import { opening } from "./components/opening.js";
 import { type } from "./components/typing.js";
 
 showMenu();
-if (window.innerWidth > 479) {
+if (window.innerWidth > 767) {
   opening();
-} else {
+} else if (window.innerWidth < 479) {
   const headerRight = document.querySelector(".header-right");
   const headerH1 = document.querySelector("h1");
   const intro = document.querySelector(".intro");
   const tl = new TimelineMax();
   tl.fromTo(headerRight, 0.2, { opacity: 1, x: 0 }, { opacity: 1, x: 0 })
     .fromTo(headerRight, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -50 })
-    .fromTo(headerH1, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -80 }, "-=1")
+    .fromTo(headerH1, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -70 }, "-=1")
     .fromTo(intro, 1, { opacity: 0, y: 30 }, { opacity: 1, y: 0 });
+} else {
+  const socialIcons = document.querySelector("#social-icons");
+  const menu = document.querySelector("#menu");
+  const headerRight = document.querySelector(".header-right");
+  const headerH1 = document.querySelector("h1");
+  const intro = document.querySelector(".intro");
+  const arrow = document.querySelector("#bouncing-arrow");
+  const tl = new TimelineMax();
+  tl.fromTo(headerRight, 0.2, { opacity: 1, x: 0 }, { opacity: 1, x: 0 })
+    .fromTo(headerRight, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -80 })
+    .fromTo(headerH1, 1, { opacity: 1, x: 0 }, { opacity: 1, x: -80 }, "-=1")
+    .fromTo(intro, 1, { opacity: 0, y: 30 }, { opacity: 1, y: 0 })
+    .fromTo(menu, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 })
+    .fromTo(socialIcons, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=1")
+    .fromTo(arrow, 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=1");
 }
 
 type();
